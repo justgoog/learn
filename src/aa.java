fce7db1dd7ea345c61d87c495fa29dcd2f0e8126
justgoog <2406649484@qq.com>
2024-09-03 18:21:11 +0800

    6

---
+++ src/aa.java
@@ -0,0 +1,19 @@
+
+public class aa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		double b=1;
+		float c=1;
+		short d=1;
+		byte e=1;
+		System.out.print(+a +"\r\n"+b +"\r\n"+c +"\r\n"+d +"\r\n"+e);
+
+
+	}
+
+}
